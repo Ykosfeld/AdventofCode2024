79541cd8d523567c8cb07d7019a6a7560e4da78d
Ykosfeld <yuri.kosfeld@gmail.com>
2024-12-01 11:12:33 -0300

    part two solution

--- day01/solution.py
+++ day01/solution.py
@@ -1,3 +1,5 @@
+import collections
+
 class part_one:
     left = list()
     right = list()
@@ -15,4 +17,26 @@ class part_one:
     for l, r in zip(left, right):
         sum += abs(l - r)
     
+    # print(sum)
+
+class part_two:
+    left = list()
+    right = list()
+    sum = 0
+
+    for linha in open("input2.txt", "r"):
+        entrada = linha.rstrip("\n").split("   ")
+        left.append(entrada[0])
+        right.append(entrada[1])
+    
+    
+    right_count = collections.Counter(right)
+    left_count = collections.Counter(left)
+    print(left_count.values())
+    
+    inter = set(left).intersection(right)
+
+    for key in inter:
+        sum += int(key) * right_count[key] * left_count[key]
+
     print(sum)
